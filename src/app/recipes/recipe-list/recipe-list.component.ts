61f999f8d96d43c272de7ec9ab84ce78c3f87c07
Piotr Trysinski <ptrysinski@gmail.com>
2022-02-08 14:12:56 +0100

    Managing Recipes in a Recipe Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,14 +9,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'Test Description', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-    new Recipe('Test Recipe 2', 'Test Description 2', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe){
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,13 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService{
-    
+    private recipes: Recipe[] = [
+        new Recipe('Test Recipe', 'Test Description', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+        new Recipe('Test Recipe 2', 'Test Description 2', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+      ];
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+
 }
